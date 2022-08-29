51de16ea11a02614a335981c18b1cd02541fdbb7
John Maclean <tribute2johnny@gmail.com>
2022-08-29 18:46:58 +0100

    initial commit

---
+++ games.js
@@ -0,0 +1,23 @@
+
+
+// 1: the express router
+
+// 2: The client and server are split into different folders and can both be run seperately.
+//    The client folder handles all the frontend code and.
+//    The server folder handles all the backend code and is responsible for server config.
+
+// 3: The server.js file handles the http requests.
+
+// 4: I couldn't find a games router but if it's the create router file. it is responsible for
+//    handling all the methods for the api data.
+
+// 5: The client makes requests to the server which the server then responds to.
+
+// 6: The second fetch method is an init object that allows you to control a number of settings
+//    in this case i think it uses a post method to add a new game to the db.
+
+// 7: get games, delete games, create games and post games 
+
+// 8: We are using the mongo db driver for our crud operations. To store all non relational data in a database.
+
+// 9: objectId gives each object in the database a unique id
